036ef55718cf9b4cfc5707d37d96a528faba31a6
Kletss <kletss@mail.ru>
2021-04-08 18:06:07 +0300

    ДЗ к уроку 4

---
+++ 3l_КлецСергей/VKApp/View/Reusable/GroupsAllCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupsAllCell.swift
+//  VKApp
+//
+//  Created by KKK on 06.04.2021.
+//
+
+import UIKit
+
+class GroupsAllCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+}
